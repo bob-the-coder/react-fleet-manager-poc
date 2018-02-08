d2eb72c917c3af89a6b4d5165deedea31d0f31a2
calin sicoe <calinsicoe@gmail.com>
2018-02-08 20:40:08 +0200

    remove existing components. start restructuring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './css/input.css';
-import './css/table.css';
+import './theme.css';
 
 import {
   Route,
@@ -10,26 +9,16 @@ import {
   HashRouter
 } from "react-router-dom";
 
-import InquiryForm from './home/InquiryForm.js'
-import Tabs from './Tabs.jsx';
-import Menu from './js/menu.js';
-import TripTable from './trips/trips.js';
-
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <div className="app">
-          <Menu />
           <header className="app-header">
             <img src={logo} className="app-logo" alt="logo" />
             <span className="app-title">R!der</span>
           </header>
           <div className="app-body">
-            <Route exact path='/' component={Tabs}/>
-            <Route exact path='/plan' component={InquiryForm}/>
-            <Route exact path='/trips/planned' component={TripTable} />
-            <Route exact path='/trips/upcoming' component={TripTable} />
           </div>
         </div>
       </HashRouter>
